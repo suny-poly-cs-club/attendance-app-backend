61968ea1310d738d63cfcebb5bf06fc4893e93c4
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-09-19 13:59:58 -0400

    start

--- src/index.js
+++ src/index.js
@@ -12,6 +12,27 @@ app
   .then(addr => {
     console.log("Listening on", addr);
   });
+  
+	
+
+app.post("/sign-up",(request,reply) =>{
+	
+});
+
+app.post("/login",(request,reply) =>{
+	replay.code(403);
+	return '{"status":"ERROR")';
+});
+
+app.post("/check-in", (request,reply) =>{
+	var token = request.body.token;
+	var success  = checkIn(token);
+	return '{"success": '+success+'}';
+});
+
+function checkIn(token){
+	return false;
+}
 
 // for some reason, sending SIGINT through Docker
 // doesn't kill it without this

def48c66a3f99498a14ab0ec320c70c7a419608c
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-10-17 13:39:21 -0400

    start singup inputs

--- src/posted.js
+++ src/posted.js
@@ -3,11 +3,21 @@ import {Database} from './database.js';
 const database = new Database();
 export const postEndpoints = (app, options ,done) =>{
 	app.post("/sign-up",(request,reply) =>{
+		//email
+		//first name
+		//last name
+		//student id
+		//password
+		let email = requet.email;
+		let firstName = request.firstName;
+		let lastName = request.lastName;
+		let id = request.studentID;
+		let password = request.password;
 		reply.code(500);
 		return '{"status":"ERROR"}';
 	});
 	
-	app.post("/login",(request,reply) =>{
+	app.post("/login",(request,reply) =>{	
 		let userEmail = request.email;
 		let rawPassword = request.password;
 		let saltedPassword = saltPassword(rawPassword);
@@ -41,19 +51,6 @@ export const postEndpoints = (app, options ,done) =>{
 		return '<html> 	<head> 		<style> 			.main { 				margin: 1%; 				height: 98%; 			} 			.inputs { 				background: lightgray; 				padding: 1%; 			} 			.responce { 				padding: 0.5%; 				font-size: larger; 				width: 99%; 				background: lightgray; 				height: 60%; 			} 			.status { 				font-size: larger; 			} 			table.inputTable { 				border-spacing: 1em; 			} 			input.inputBox { 				width: 500%; 				padding: 5%; 				border-radius: 1em; 				border-width: 1px; 				font-size: medium; 			} 		</style> 		<script> 			function postit(){\nvar ajaxRequest = new XMLHttpRequest();\najaxRequest.onreadystatechange = function(){\nif(ajaxRequest.readyState == 4){\ndocument.querySelector(".responce").innerHTML=ajaxRequest.responseText;\ndocument.querySelector(".statusCode").innerHTML="status: "+ajaxRequest.status;\n}\n}\najaxRequest.open("POST", document.querySelector(".url").value);\najaxRequest.setRequestHeader("Content-type", document.querySelector(".contentType").value);\najaxRequest.send(document.querySelector(".data").value);\n}\n</script> 	</head> 	 	<body> 		<div class="main"> 			<div class="inputs"> 				<table class="inputTable"> 					<tr> 						<td>URL to post to:</td> 						<td><input class="url inputBox"/></td> 					</tr> 					<tr> 						<td>Post data:</td> 						<td><input class="data inputBox"/></td> 					</tr> 					<tr> 						<td>Content type:</td> 						<td><input class="contentType inputBox"/></td> 					</tr> 				</table> 				 				<button onclick="postit()" style="padding: .5%;width: 70%;background: red;font-weight: bold;"> POST </button> 			</div> 			<div class="status"> 				<br> 				<span class="statusCode">status:</span> 				<br><br>responce: 			</div> 			<div class="responce"> 			 			</div> 		</div> 	</body> </html>';
 	});
 	done();
-}
-
-function numericalHash(string){
-  var value =0;
-  for(var i=0;i<string.length;i++){
-      value += string.charCodeAt(i)*Math.pow(31,i);
-  }
-  return value;
-}
-
-function saltPassword(password){
-	let saltRounds = 8;
-	//return bcrypt.hashSync(saltRounds,password);
 }
 
 function generateNewToken(){

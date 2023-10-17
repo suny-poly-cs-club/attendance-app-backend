89c3bc7237a28e26d77b5bc6a3a8cfa9fedaa16d
jSdCool <37940266+jSdCool@users.noreply.github.com>
2023-10-17 13:17:11 -0400

    ddiaable password things

--- src/posted.js
+++ src/posted.js
@@ -36,7 +36,7 @@ export const postEndpoints = (app, options ,done) =>{
 		return '{"success": '+success+'}';	
 	});
 	
-	app.get("/gopost", (request,replay) =>{
+	app.get("/gopost", (request,reply) =>{
 		reply.type("text/html");
 		return '<html> 	<head> 		<style> 			.main { 				margin: 1%; 				height: 98%; 			} 			.inputs { 				background: lightgray; 				padding: 1%; 			} 			.responce { 				padding: 0.5%; 				font-size: larger; 				width: 99%; 				background: lightgray; 				height: 60%; 			} 			.status { 				font-size: larger; 			} 			table.inputTable { 				border-spacing: 1em; 			} 			input.inputBox { 				width: 500%; 				padding: 5%; 				border-radius: 1em; 				border-width: 1px; 				font-size: medium; 			} 		</style> 		<script> 			function postit(){\nvar ajaxRequest = new XMLHttpRequest();\najaxRequest.onreadystatechange = function(){\nif(ajaxRequest.readyState == 4){\ndocument.querySelector(".responce").innerHTML=ajaxRequest.responseText;\ndocument.querySelector(".statusCode").innerHTML="status: "+ajaxRequest.status;\n}\n}\najaxRequest.open("POST", document.querySelector(".url").value);\najaxRequest.setRequestHeader("Content-type", document.querySelector(".contentType").value);\najaxRequest.send(document.querySelector(".data").value);\n}\n</script> 	</head> 	 	<body> 		<div class="main"> 			<div class="inputs"> 				<table class="inputTable"> 					<tr> 						<td>URL to post to:</td> 						<td><input class="url inputBox"/></td> 					</tr> 					<tr> 						<td>Post data:</td> 						<td><input class="data inputBox"/></td> 					</tr> 					<tr> 						<td>Content type:</td> 						<td><input class="contentType inputBox"/></td> 					</tr> 				</table> 				 				<button onclick="postit()" style="padding: .5%;width: 70%;background: red;font-weight: bold;"> POST </button> 			</div> 			<div class="status"> 				<br> 				<span class="statusCode">status:</span> 				<br><br>responce: 			</div> 			<div class="responce"> 			 			</div> 		</div> 	</body> </html>';
 	});
@@ -52,8 +52,8 @@ function numericalHash(string){
 }
 
 function saltPassword(password){
-	let saltRounds = 16;
-	return bcrypt.genSalt(saltRounds,password);
+	let saltRounds = 8;
+	//return bcrypt.hashSync(saltRounds,password);
 }
 
 function generateNewToken(){
